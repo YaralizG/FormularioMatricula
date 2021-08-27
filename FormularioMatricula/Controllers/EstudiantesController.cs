1ba7aaa0febd5f36be507f2ce54de0a6c774b0ac
yaraliz <ygm641@gmail.com>
2021-08-27 03:47:29 -0500

    Registro

--- FormularioMatricula/Controllers/EstudiantesController.cs
+++ FormularioMatricula/Controllers/EstudiantesController.cs
@@ -6,10 +6,26 @@ namespace FormularioMatricula.Controllers
 {
     public class EstudiantesController: Controller
     {
+        private readonly AplicationDbContext _context;
+
+        public EstudiantesController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Registrar(Estudiantes objEstudiantes)
+        {
+            _context.Add(objEstudiantes);
+            _context.SaveChanges();
+            ViewData["Message"] = "Estudiante registrado";
+            return View("Index");
+        }
+        
 
         
     }
--- FormularioMatricula/Models/Estudiantes.cs
+++ FormularioMatricula/Models/Estudiantes.cs
@@ -4,13 +4,32 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FormularioMatricula.Models
 {
+
+    [Table("t_alumno")]
+
     public class Estudiantes
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        
+        [Column("id")]
+        public int id{get;set;}
+
+        [Column("apellidos")]
         public string apellido{get;set;}
+
+        [Column("nombres")]
         public string nombre{get;set;}
+
+        [Column("fechanacimiento")]
         public DateTime fechanacimiento{get;set;}
+        
+        [Column("dni")]
         public string dni{get;set;}
+
+        [Column("genero")]
         public string genero{get;set;}
+
+        [Column("carrera")]
         public string carrera{get;set;}
         
     }
